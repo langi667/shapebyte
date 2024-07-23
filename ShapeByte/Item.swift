0d96f1e9c3940d2818888f99bac441971710be5b
Stefan Lang <stefan.lang@rtl.de>
2024-07-23 19:37:51 +0200

    Initial Commit

---
+++ ShapeByte/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ShapeByte
+//
+//  Created by Lang, Stefan [RTL Tech] on 23.07.24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ShapeByte/ShapeByteApp.swift
@@ -0,0 +1,32 @@
+//
+//  ShapeByteApp.swift
+//  ShapeByte
+//
+//  Created by Lang, Stefan [RTL Tech] on 23.07.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ShapeByteApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
