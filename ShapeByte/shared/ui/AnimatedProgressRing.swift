eb4ab3c562aff9d55be2a86ad7bca669106bdf8c
Stefan Lang <stefan.lang@rtl.de>
2024-07-24 08:44:01 +0200

    Initial commit

---
+++ ShapeByte/shared/ui/AnimatedProgressRing.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedProgressRing.swift
+//  ShapeByte
+//
+//  Created by Lang, Stefan [RTL Tech] on 23.07.24.
+//
+
+import SwiftUI
+
+struct AnimatedProgressRing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedProgressRing()
+}
