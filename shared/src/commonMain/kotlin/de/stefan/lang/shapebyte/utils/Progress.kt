e3c44c895fc1440f5d8fdcba892491fad1a3ef8f
Stefan Lang <stefan.lang@rtl.de>
2024-09-24 20:26:12 +0200

    SB-5: add Testing infrastructure

---
+++ shared/src/commonMain/kotlin/de/stefan/lang/shapebyte/utils/Progress.kt
@@ -0,0 +1,23 @@
+package de.stefan.lang.shapebyte.utils
+
+import kotlin.math.roundToInt
+
+data class Progress(private val progress: Float) {
+    companion object {
+        val ZERO = Progress(0f)
+        val COMPLETE = Progress(1f)
+    }
+
+    val value: Float = adjustProgress(progress)
+    val absoluteValue: Int by lazy { (value * 100).roundToInt() }
+
+    constructor(value: Int) : this(value.toFloat() / 100)
+
+    private fun adjustProgress(value: Float): Float {
+        return when {
+            value < 0 -> 0f
+            value > 1 -> 1f
+            else -> value
+        }
+    }
+}
---
+++ shared/src/commonTest/kotlin/de/stefan/lang/shapebyte/utils/ProgressTest.kt
@@ -0,0 +1,47 @@
+import de.stefan.lang.shapebyte.utils.Progress
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ProgressTest {
+    @Test
+    fun `value should be correctly adjusted`() {
+        var sut = Progress(0)
+        assertEquals(0f, sut.value)
+
+        sut = Progress(-1f)
+        assertEquals(0f, sut.value)
+
+        sut = Progress(0.53f)
+        assertEquals(0.53f, sut.value)
+
+        sut = Progress(1f)
+        assertEquals(1f, sut.value)
+
+        sut = Progress(1.3f)
+        assertEquals(1f, sut.value)
+    }
+
+    @Test
+    fun `absoluteValue should be between 0 and 100`() {
+        var sut = Progress(0)
+        assertEquals(0, sut.absoluteValue)
+
+        sut = Progress(-0.3f)
+        assertEquals(0, sut.absoluteValue)
+
+        sut = Progress(0.53f)
+        assertEquals(53, sut.absoluteValue)
+
+        sut = Progress(1f)
+        assertEquals(100, sut.absoluteValue)
+
+        sut = Progress(1.3f)
+        assertEquals(100, sut.absoluteValue)
+    }
+
+    @Test
+    fun `Defined values must be correct`() {
+        assertEquals(0f, Progress.ZERO.value)
+        assertEquals(1f, Progress.COMPLETE.value)
+    }
+}
