17eb082bb15c4ab4d0c88557760e9821a3f8f9ca
Stefan Lang <stefan.lang@rtl.de>
2024-11-17 18:42:06 +0100

    SB-44 AssetLoader can load image ressources on Android

--- scripts/ci-android.sh
+++ scripts/ci-android.sh
@@ -6,6 +6,17 @@ cd "$script_directory/../" || exit 200
 source "$script_directory/core/logging.sh"
 source "$script_directory/core/error_codes.sh"
 
+### Gradle Update ###
+andLogI "Updating Project via gradle ..."
+./gradlew --refresh-dependencies
+
+return_code=$?
+if [ $return_code -ne 0 ]; then
+  # shellcheck disable=SC2086
+  exit $ERROR_ANDROID_GRADLE_UPDATE_FAILED
+fi
+
+
 ### Lint / Detekt ###
 "$script_directory/lint-android.sh"
 return_code=$?
@@ -20,7 +31,14 @@ return_code=$?
 if [ $return_code -ne 0 ]; then
   # shellcheck disable=SC2086
   exit $ERROR_ANDROID_UNIT_TEST_FAILED
-else
-  exit 0
 fi
 
+### Android instrumented tests ###
+"$script_directory/android-tests.sh"
+return_code=$?
+if [ $return_code -ne 0 ]; then
+  # shellcheck disable=SC2086
+  exit $ERROR_ANDROID_INSTRUMENTED_TESTS_FAILED
+fi
+
+exit 0
--- shared/src/androidMain/kotlin/de/stefan/lang/shapebyte/utils/assets/impl/AssetLoader.kt
+++ shared/src/androidMain/kotlin/de/stefan/lang/shapebyte/utils/assets/impl/AssetLoader.kt
@@ -1,13 +1,29 @@
 package de.stefan.lang.shapebyte.utils.assets.impl
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import de.stefan.lang.shapebyte.utils.assets.AssetLoading
 import de.stefan.lang.shapebyte.utils.assets.FileAsset
+import de.stefan.lang.shapebyte.utils.assets.ImageAsset
 import de.stefan.lang.shapebyte.utils.logging.Loggable
 import de.stefan.lang.shapebyte.utils.logging.Logging
+import java.io.IOException
+
+interface AssetLoaderAndroid {
+    fun loadImageAsset(asset: ImageAsset, context: Context): Bitmap? {
+        return try {
+            val assetManager = context.assets
+            BitmapFactory.decodeStream(assetManager.open("images/${asset.assetName}"))
+        } catch (_: IOException) {
+            null
+        }
+    }
+}
 
 actual class AssetLoader actual constructor(logging: Logging) :
     AssetLoading,
+    AssetLoaderAndroid,
     Loggable {
     actual override val logger = logging
     private lateinit var context: Context
@@ -17,6 +33,12 @@ actual class AssetLoader actual constructor(logging: Logging) :
     }
 
     actual override fun loadFile(fileAsset: FileAsset): String {
-        return context.assets.open(fileAsset.subPath).bufferedReader().use { it.readText() }
+        val retVal: String = try {
+            context.assets.open(fileAsset.subPath).bufferedReader().use { it.readText() }
+        } catch (ex: IOException) {
+            logE("Error loading file: ${fileAsset.subPath} with error: $ex")
+            ""
+        }
+        return retVal
     }
 }
