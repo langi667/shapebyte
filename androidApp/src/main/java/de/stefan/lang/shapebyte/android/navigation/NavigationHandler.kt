93bb9e716d4781374527b2935590561bc241e837
Stefan Lang <stefan.lang@yazio.com>
2026-02-05 20:53:20 +0100

    SB-84 basic 9.0.0 gradle upgrade

--- androidApp/src/main/java/de/stefan/lang/shapebyte/android/navigation/NavigationHandler.kt
+++ androidApp/src/main/java/de/stefan/lang/shapebyte/android/navigation/NavigationHandler.kt
@@ -3,14 +3,14 @@ package de.stefan.lang.shapebyte.android.navigation
 import androidx.navigation.NavHostController
 import de.stefan.lang.shapebyte.SharedModule
 import de.stefan.lang.shapebyte.features.navigation.contract.NavigationRequest
-import de.stefan.lang.shapebyte.features.navigation.contract.NavigationRequestHandling
+import de.stefan.lang.shapebyte.features.navigation.contract.NavigationRequestHandler
 import de.stefan.lang.utils.logging.contract.Loggable
 import de.stefan.lang.utils.logging.contract.Logger
 
 class NavigationHandler(
     private val navHostController: NavHostController,
     override val logger: Logger = SharedModule.logger(),
-) : NavigationRequestHandling, Loggable {
+) : NavigationRequestHandler, Loggable {
 
     override fun handleNavigationRequest(request: NavigationRequest) {
         when (request) {
--- features/workout/presentation/contract/src/commonMain/kotlin/de/stefan/lang/shapebyte/features/workout/contract/WorkoutPresentationContract.kt
+++ features/workout/presentation/contract/src/commonMain/kotlin/de/stefan/lang/shapebyte/features/workout/contract/WorkoutPresentationContract.kt
@@ -1,10 +1,10 @@
 package de.stefan.lang.shapebyte.features.workout.contract
 
-import de.stefan.lang.shapebyte.features.navigation.contract.NavigationRequestHandling
+import de.stefan.lang.shapebyte.features.navigation.contract.NavigationRequestHandler
 import de.stefan.lang.shapebyte.features.workout.contract.countdown.CountdownItemSetsViewModel
 import de.stefan.lang.shapebyte.features.workout.contract.timed.TimedWorkoutViewModel
 
 public interface WorkoutPresentationContract {
     public fun countdownItemSetsViewModel(): CountdownItemSetsViewModel
-    public fun timedWorkoutViewModel(navHandler: NavigationRequestHandling): TimedWorkoutViewModel
+    public fun timedWorkoutViewModel(navHandler: NavigationRequestHandler): TimedWorkoutViewModel
 }
